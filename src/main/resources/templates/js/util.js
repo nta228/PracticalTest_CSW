f4ccfa73ae2eb972993fe7c7085098ed1b749d33
nta228 <64982003+nta228@users.noreply.github.com>
2022-08-20 00:43:27 +0700

    Delete src/main/resources/templates directory

--- src/main/resources/templates/js/util.js
+++
@@ -1,7 +0,0 @@
-var API_URL = "https://http://localhost:8080/employees"
-
-function getParameterFromUrl(name){
-    var url_string = window.location.href;
-    var url = new URL(url_string);
-    return url.searchParams.get(name);
-}
